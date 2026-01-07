f05c285819179c4a572ea2987e98041800aaa059
Devendra Pandey <pandey.devendra@indiamart.com>
2026-01-07 17:48:59 +0530

    feat: Implement initial Next.js app structure with Clerk authentication, theme provider, custom hooks, and global styling.

--- src/hooks/use-current-theme.js
+++ src/hooks/use-current-theme.js
@@ -1,6 +1,12 @@
 import { useTheme } from "next-themes";
 
-export const useCurrentTheme = () => {
-    const { theme, systemTheme } = useTheme();
-    return theme === "system" ? systemTheme : theme;
-};
+
+export const useCurrentTheme = ()=>{
+    const {theme , systemTheme} = useTheme();
+
+    if(theme === "dark" || theme==="light"){
+        return theme;
+    }
+
+    return systemTheme;
+}
