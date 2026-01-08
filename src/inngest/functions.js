32dae10fc3d792511accdc86c6dc6140cebb3012
Devendra Pandey <pandey.devendra@indiamart.com>
2026-01-08 17:07:24 +0530

    E2B sandbox setup

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -1,10 +1,40 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
+import Sandbox from "@e2b/code-interpreter";
+
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
-  { event: "test/hello.world" },
+  { event: "agent/hello" },
+
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello World!` };
+
+    const sandBoxId = await step.run("get-sandbox-id", async () => {
+        const sandbox = await Sandbox.create("v0-clone-nextjs-app");
+        return sandbox.sandboxId;
+    })
+
+    const helloAgent = createAgent({
+      name: "hello-agent",
+      description: "A simple agent that says hello",
+      system: "You are a helpful assistant.",
+      model: gemini({
+        model: "gemini-2.5-flash",
+      }),
+    });
+
+    const {output} = await helloAgent.run("Say hello to the user");
+
+    const sandBoxUrl = await step.run("get-sandbox-url", async () => {
+        const sandbox = await Sandbox.connect(sandBoxId);
+        const host = sandbox.getHost(3000);
+        return `http://${host}`;
+    })
+
+    return {
+        message: output[0].content
+    }
+
   },
 );
