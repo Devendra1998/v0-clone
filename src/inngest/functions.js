6428a542f15c271b9a81d8890fe9e32110a8f8a5
Devendra Pandey <pandey.devendra@indiamart.com>
2026-01-12 14:47:44 +0530

    inngest agent memory

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -20,13 +20,38 @@ export const codeAgentFunction = inngest.createFunction(
       return sandbox.sandboxId;
     })
 
+    const previousMessages = await step.run(
+      "get-previous-messages",
+      async () => {
+        const formattedMessages = [];
+
+        const messages = await db.message.findMany({
+          where: {
+            projectId: event.data.projectId,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        });
+
+        for (const message of messages) {
+          formattedMessages.push({
+            type: "text",
+            role: message.role === "ASSISTANT" ? "assistant" : "user",
+            content: message.content,
+          });
+        }
+        return formattedMessages;
+      }
+    );
+
     const state = createState(
       {
         summary: "",
         files: {},
       },
       {
-        messages: null,
+        messages: previousMessages,
       }
     );
 
@@ -160,7 +185,7 @@ export const codeAgentFunction = inngest.createFunction(
       },
     });
 
-    const result = await network.run(event.data.value);
+    const result = await network.run(event.data.value, {state});
 
     const fragmentTitleGenerator = createAgent({
       name: "fragment-title-generator",
